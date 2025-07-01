ea049645ec7ae87a9e4e190136f54ace5d5eae2e
cd0g05 <carter.cripe@gmail.com>
2025-07-01 14:59:59 -0700

    edited descision prompt

--- src/llm.py
+++ src/llm.py
@@ -111,7 +111,7 @@ class OpenaiAiService(AiService):
         Previous message 1: {pm2.author}: "{pm2.content}"
         Previous message 2: {pm1.author}: "{pm1.content}"
         Most recent message: {um.author}: "{um.content}"
-        Determine which type of response fits the best, and respond with a single integer, 1-5, where 1 = disagreement, 2 = haiku, 3 = bible verse, 4 = art of war, and if the message is not conducive to any option return 5. Respond only with a single integer 1-5"""
+        Determine which type of response fits the best, and respond with a single integer, 1-5, where 1 = disagreement, 2 = haiku, 3 = bible verse, 4 = art of war. If the message is really not conducive to any option (be quite liberal), return 5. Respond only with a single integer 1-5"""
         response = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
